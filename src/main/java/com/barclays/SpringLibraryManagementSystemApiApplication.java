d7745facd55a073329ea7c389220cc4447afdd4a
shonanohs <shonafitzpatrick15@gmail.com>
2023-11-17 09:59:33 +0000

    Initial commit

---
+++ src/main/java/com/barclays/SpringLibraryManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.barclays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLibraryManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLibraryManagementSystemApiApplication.class, args);
+	}
+
+}
